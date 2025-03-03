1a1cd99df26149ec92507d5cfb99d286ccb09d9d
MoMosaad2000 <mohameedmosaad5@gmail.com>
2025-03-03 14:59:42 +0300

    vite.config

--- invetory/vite.config.js
+++ invetory/vite.config.js
@@ -2,19 +2,15 @@
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    // قل لـ Vite إن الجذر هو المجلد public
     root: 'public',
-
     plugins: [react()],
-
     build: {
-        // سيُنتِج ملفات البناء النهائي في مجلد dist أعلى public
         outDir: '../dist',
         emptyOutDir: true,
     },
     server: {
-        // إذا أردت ضبط المنفذ أو أي إعدادات أخرى
-        port: 3000,
-        strictPort: true
+        proxy: {
+            '/api': 'http://inventory2025.runasp.net/'
+        }
     }
 })
